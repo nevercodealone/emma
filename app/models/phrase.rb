f21f2094f0b94e5ee400e66c1c1116a10a5a9fba
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2017-06-10 14:06:01 +0200

    Implemented automatic translation

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,5 +1,20 @@
 class Phrase < ApplicationRecord
   belongs_to :category
   belongs_to :language
+
+  before_save :fill_translation
+
+  def fill_translation
+    if self.translation.blank?
+      translate = Google::Apis::TranslateV2::TranslateService.new
+      translate.key = GOOGLE_API_KEY
+      result = translate.list_translations(self.text, self.language.key, source: 'de', format: 'text')
+      if result.translations.count > 0
+        self.translation = result.translations.first.translated_text
+      end
+    end
+
+    true
+  end
 end
 
