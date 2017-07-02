5d2d9480391a875724b002ce9864cfff51aa483a
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2017-07-02 14:09:25 +0200

    Added routes for translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :backend do
     resources :categories do
-      resources :phrases
+      resources :phrases do
+        member do
+          post 'update_translations'
+        end
+        resources :translations
+      end
     end
     resources :languages
     root to: 'categories#index'
