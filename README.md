8188c718820fc256d63f686a06efaf00fe82fb2f
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2017-12-04 22:28:57 +0100

    Updated binstubs for Rails 5

--- README.md
+++ README.md
@@ -13,6 +13,22 @@ docker-compose up --build
 # starts the devserver, point a browser to http://localhost:3000
 ```
 
+## Dump the database
+
+There is the new and convenient way to dump the current production database.
+Just say:
+
+```
+bundle exec cap production dump:down
+```
+
+This should give you a file called gymbot.dump.tar in the projects main directory.
+To import this dump run
+
+```
+bundle exec rake db:restore
+```
+
 # License
 
 This code is free as in beer.
--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -14,7 +14,13 @@ class Phrase < ApplicationRecord
           translation.text = result.translations.first.translated_text
           translation.save
 
-          translation.text.to_file(language.key, File.join(Rails.root, 'public', 'uploads', 'tts', "translation-#{translation.id}-#{language.key}.mp3").to_s)
+          supported_languages = ['af', 'ar', 'az', 'be', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'en', 'en_us', 'en_gb', 'en_au', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'ga', 'gl', 'gu', 'hi', 'hr', 'ht', 'hu', 'id', 'is', 'it', 'iw', 'ja', 'ka', 'kn', 'ko', 'la', 'lt', 'lv', 'mk', 'ms', 'mt', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'vi', 'yi', 'zh', 'zh-CN', 'zh-TW']
+          if supported_languages.include? language.key
+            begin
+              translation.text.to_file(language.key, File.join(Rails.root, 'public', 'uploads', 'tts', "translation-#{translation.id}-#{language.key}.mp3").to_s)
+            rescue SystemExit
+            end
+          end
         end
       end
     end
